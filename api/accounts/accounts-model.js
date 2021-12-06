788b9dcb759b3e4b77af74e4510f0e6ae5eca663
Dave Madden <davelearnsstuff@gmail.com>
2021-12-06 13:19:43 -0800

    router done, model halfway done

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,21 +1,33 @@
-const getAll = () => {
+const db = require('../../data/db-config')
+
+const getAll = async () => {
   // DO YOUR MAGIC
+  const rows = await db('accounts')
+  return rows;
 }
 
-const getById = id => {
+const getById = async id => {
   // DO YOUR MAGIC
+  const record = await db('accounts')
+    .where("id", id)
+  return record
 }
 
-const create = account => {
-  // DO YOUR MAGIC
+const create = async account => {
+  const thing = await db('accounts')
+    .insert(account)
+  
+  return await getById(thing)
 }
 
-const updateById = (id, account) => {
+const updateById = async (id, account) => {
   // DO YOUR MAGIC
+  return "updating by id"
 }
 
-const deleteById = id => {
+const deleteById = async id => {
   // DO YOUR MAGIC
+  return "deleting by id"
 }
 
 module.exports = {
